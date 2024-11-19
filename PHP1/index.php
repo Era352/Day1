67707fc3257014efa5a399aa3024c33895aa2688
Era352 <era.kryeziu787@gmail.com>
2024-11-19 19:24:51 +0100

    Add files via upload

---
+++ PHP1/index.php
@@ -0,0 +1,23 @@
+<?php
+    $school = "Digital School";
+    //echo "I love $school";
+
+    //int, float, string, boolean, array, object, resources, null
+
+  // simple arithmetic's +, -, *, /, %
+    $x = 120;
+    $y = 50;
+    //echo $x + $y;
+    //echo $x - $y;
+    //echo $x * $y;
+    //echo $x / $y;
+
+    //string
+
+    $a="Digital";
+    $b="School";
+    $c=$a.$b;
+    echo "$c \n";
+?>
+
+/*variables local gloal static*/ 
